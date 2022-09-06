39414b40d1d87cbad1f664db9d8e9723d5b6d949
Joe <erreyjoe@gmail.com>
2022-09-07 00:26:09 +0100

    Moved test function to REPL

--- main.cpp
+++ main.cpp
@@ -5,38 +5,6 @@
 #include "Evaluator.h"
 #include "REPL.h"
 
-
-void test(){
-    std::map<std::string, int> tests {{"3-(2+1)", 0}, {"2+2", 4}, {"((((((1+1)+1)+1)+1)+1)+1)", 7}, {"1*5", 5}, {"2*7+2", 16}, {"(3+4)*(4/2)", 14}, {"2 * 54 + 2 * (7 + 5) + 4/2", 134}};
-    int i = 1;
-    for(auto test: tests){
-        Lexer l(test.first);
-        std::vector<Token> tokens = l.get_tokens();
-        ShuntYard sy(tokens);
-        std::vector<Object> objects = sy.to_infix();
-        Evaluator e(objects);
-        int result = e.get_result();
-        if(result != test.second){
-            std::cout << "test #" << i << " failed. Got " << result << " expected " << test.second << "\n";
-            std::cout << "Tokens: ";
-
-            for(Token t: tokens){
-                std::cout << "{" << t.get_type() << " '" << t.get_literal() << "'}, ";
-            }
-
-            std::cout << "Objects: ";
-
-            for(Object obj: objects){
-                std::cout << "{" << obj.literal << "}, ";
-            }
-
-        }else{
-            std::cout << "test #" << i << " passed.\n";
-        }
-        i++;
-    }
-}
-
 int main() {
     REPL repl;
 
