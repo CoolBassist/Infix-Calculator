e99f31df2c7ef6e946134f617c0de8f098dea03f
Joe <erreyjoe@gmail.com>
2022-09-18 15:48:34 +0100

    Refactored for new Object classes.

--- ShuntYard.cpp
+++ ShuntYard.cpp
@@ -3,6 +3,7 @@
 //
 
 #include <iostream>
+#include <variant>
 #include "ShuntYard.h"
 
 ShuntYard::ShuntYard(std::vector<Token> tokens) {
@@ -10,50 +11,45 @@ ShuntYard::ShuntYard(std::vector<Token> tokens) {
     position = 0;
 }
 
-std::vector<Object> ShuntYard::to_infix() {
-    std::vector<Object> objects;
+std::vector<std::variant<IntObject, OpObject>> ShuntYard::to_infix() {
+    std::vector<std::variant<IntObject, OpObject>> objects;
 
     while (position < tokens.size()) {
         switch (tokens[position].get_type()) {
             case NEG:
                 position++;
-                output.push(Object(INT, 0-std::stoi(tokens[position].get_literal()), "-" + tokens[position].get_literal()));
+                output.push(IntObject(0-std::stoi(tokens[position].get_literal()), "-" + tokens[position].get_literal()));
                 break;
             case INT:
-                output.push(Object(INT, std::stoi(tokens[position].get_literal()), tokens[position].get_literal()));
+                output.push(IntObject(std::stoi(tokens[position].get_literal()), tokens[position].get_literal()));
                 break;
             case ADD:
             case SUB:
                 while (operators.size() != 0 &&
-                        (operators.top().literal == "*" || operators.top().literal == "/")) {
-                    Object cur_object = operators.top();
+                        (operators.top().get_literal() == "*" || operators.top().get_literal() == "/")) {
+                    OpObject cur_object = operators.top();
                     output.push(cur_object);
                     operators.pop();
                 }
             case MULT:
             case DIV:
-                while(operators.size() != 0 && (operators.top().literal == "^" || operators.top().literal == "sin" || operators.top().literal == "cos" || operators.top().literal == "tan")){
-                    Object cur_object = operators.top();
+                while(operators.size() != 0 && (operators.top().get_literal() == "^")){
+                    OpObject cur_object = operators.top();
                     output.push(cur_object);
                     operators.pop();
                 }
             case EXP:
             case LPAREN:
-                operators.push(Object(tokens[position].get_type(), tokens[position].get_literal(), tokens[position].get_literal()));
+                operators.push(OpObject(tokens[position].get_type(), tokens[position].get_literal()));
                 break;
             case RPAREN:
                 if(operators.size() != 0) {
-                    while (operators.size() != 0 && operators.top().literal.compare("(") != 0) {
+                    while (operators.size() != 0 && operators.top().get_literal().compare("(") != 0) {
                         output.push(operators.top());
                         operators.pop();
                     }
                     operators.pop();
                 }
-                break;
-            case SIN:
-            case COS:
-            case TAN:
-                operators.push(Object(tokens[position].get_type(), tokens[position].get_literal(), tokens[position].get_literal()));
                 break;
         }
 
